93fa508bd9b4127ac70b49df92ac596e45c9aebe
rockontrol <mqjmqj2014@outlook.com>
2020-12-14 11:12:41 +0800

    update

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
-webdriver 下载地址：http://chromedriver.storage.googleapis.com/index.html
+青柠疫情‘半自动’打卡
+---
+基于 Python + selenium。解决青柠疫情每天的繁琐登录打卡，缺点是需要手动输入验证码（已经在想办法解决了），所以叫做半自动打卡。
+暂时凑合用用吧。
+
+
 
 ---
-正在开发，目前不可用
+使用教程：
+ - 1.安装 Python 3.6 及以上版本。（怎么安装自行百度）
+
+    
+
+ - 2.requirements.txt 是所需第三方模块，执行 
+
+  ```
+  pip install -r requirements.txt
+  ```
+
+   安装模块。
+  需要安装的 Python 库有：selenium 、 request 库。
+
+  
+
+ - 3.需要下载对应系统的 webdriver 。webdriver 下载地址：http://chromedriver.storage.googleapis.com/index.html
+ 并存放到本文件夹下。并修改代码中 webdriver 的绝对路径。
+
+ - 4.修改名字、学号、密码等变量。
+
+ - 5.运行
 
 
----
 
-
-
-安装必要库，下载 webdriver 。填写必要信息，
 
--- daka.py
+++ daka.py
@@ -1,68 +1,64 @@
 import sys
 import io
 from selenium import webdriver
+import time
+import requests
+
 # usragent:Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8') #改变标准输出的默认编码
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')  # 改变标准输出的默认编码
 
-
-#建立chrome浏览器对象，括号里是Chromedriver在你的电脑上的路径
+# 建立chrome浏览器对象，括号里是Chromedriver在你的电脑上的路径，需要修改
 browser = webdriver.Chrome('/Users/rockontrol/Desktop/daka/chromedriver')
 
-#基本信息
+# 基本信息，需要修改
 xuehao = 'xxxx'
 name = 'xxx'
-password = 123456
+password = 'xxxx'
 
-#登录页面
+# 登录页面
 url = 'https://wxyqfk.zhxy.net/?yxdm=14262#/login'
-browser.set_window_size(598,702)
+browser.set_window_size(598, 702)
 # 访问登录页面
 browser.get(url)
 
 # 等待一定时间，让js脚本加载完毕
 browser.implicitly_wait(5)
-
+time.sleep(3)
 # print(browser.page_source.encode('utf-8').decode())
-#定位输入框并输入相关信息
-browser.find_elements_by_class_name("van-field__control")[0] .send_keys(xuehao)
-browser.find_elements_by_class_name("van-field__control")[1] .send_keys(name)
-browser.find_elements_by_class_name("van-field__control")[3] .send_keys(password)
+# 定位输入框并输入相关信息
+browser.find_elements_by_class_name("van-field__control")[0].send_keys(xuehao)
+browser.find_elements_by_class_name("van-field__control")[1].send_keys(name)
+browser.find_elements_by_class_name("van-field__control")[3].send_keys(password)
 
 # 等待一定时间，让js脚本加载完毕
 browser.implicitly_wait(3)
+# browser.sleep(3)
 browser.find_element_by_xpath('//button').click()
 browser.implicitly_wait(3)
 
-#地址重定向到登陆后页面
+# 地址重定向到登陆后页面
 url2 = 'https://wxyqfk.zhxy.net/?yxdm=14262#/clockIn'
-browser.set_window_size(598,702)
+browser.set_window_size(598, 702)
 browser.get(url2)
-browser.implicitly_wait(5)
+browser.implicitly_wait(10)
 
-#关闭有个温馨提示的框
+# 关闭有个温馨提示的框
 browser.find_element_by_class_name('van-button').click()
+time.sleep(3)
+browser.find_element_by_class_name('sign-in-btn').click()
+browser.find_elements_by_xpath('//label')[0].click()
+browser.find_elements_by_xpath('//label')[6].click()
+browser.find_elements_by_xpath('//label')[14].click()
 
+time.sleep(2)
 
+# image = browser.find_element_by_tag_name('img').get_attribute('src')
+# print(image)
+# # 通过requests发送一个get请求到图片地址，返回的响应就是图片内容
+# r = requests.get(image)
+#
+# # 将获取到的图片二进制流写入本地文件
+# with open('yanzheng.png', 'wb') as f:
+#     # 对于图片类型的通过r.content方式访问响应内容，将响应内容写入png中
+#     f.write(r.content)
 
-# #输入用户名
-# username = browser.find_element_by_name('user')
-# username.send_keys('学号')
-#
-# #输入密码
-# password = browser.find_element_by_name('pwd')
-# password.send_keys('密码')
-#
-# #选择“学生”单选按钮
-# student = browser.find_element_by_xpath('//input[@value="student"]')
-# student.click()
-#
-# #点击“登录”按钮
-# login_button = browser.find_element_by_name('btn')
-# login_button.submit()
-#
-# #网页截图
-# browser.save_screenshot('picture1.png')
-# #打印网页源代码
-# print(browser.page_source.encode('utf-8').decode())
-#
-# browser.quit()
